641131147e3bf9e45f5e2fd46e74ae139e07c2b3
Yuriy <yrii.petrik@gmail.com>
2022-12-10 11:45:05 +0700

    created a new managed item

--- src/components/employees-add-form/employees-add-form.jsx
+++ src/components/employees-add-form/employees-add-form.jsx
@@ -1,20 +1,54 @@
 import React from "react"
 import "./employees-add-form.css"
+import { Component } from "react"
 
-const EmployeesAddForm = () => {
-  return (
-    <div className="app-add-form">
-      <h3>Добавьте нового сотрудника</h3>
-      <form className="add-form d-flex">
-        <input type="text" className="form-control new-post-label" placeholder="Как его зовут?" />
-        <input type="number" className="form-control new-post-label" placeholder="З/П в €?" />
+class EmployeesAddForm extends Component {
+  constructor(props) {
+    super(props)
 
-        <button type="submit" className="btn btn-outline-light">
-          Добавить
-        </button>
-      </form>
-    </div>
-  )
+    this.state = {
+      name: "",
+      salary: "",
+    }
+  }
+
+  onChangeValue = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+  render() {
+
+const {name, salary} = this.state
+
+    return (
+      <div className="app-add-form">
+        <h3>Добавьте нового сотрудника</h3>
+        <form className="add-form d-flex">
+          <input
+            type="text"
+            className="form-control new-post-label"
+            placeholder="Как его зовут?"
+						value={name}
+            name="name"
+            onChange={this.onChangeValue}
+          />
+          <input
+            type="number"
+            className="form-control new-post-label"
+            placeholder="З/П в €?"
+						value={salary}
+            name="salary"
+            onChange={this.onChangeValue}
+          />
+
+          <button type="submit" className="btn btn-outline-light">
+            Добавить
+          </button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default EmployeesAddForm
