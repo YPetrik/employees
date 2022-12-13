abb135e467949c745fb474500147e053e9838241
Yuriy <yrii.petrik@gmail.com>
2022-12-13 12:13:46 +0700

    removing employees from the list

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Component } from "react"
 import "./app.css"
 import AppInfo from "../components/app-info/app-info"
 import SearchPanel from "../components/search-panel/search-panel"
@@ -6,28 +6,42 @@ import AppFilter from "../components/app-filter/app-filter"
 import EmployeesList from "../components/employees-list/employees-list"
 import EmployeesAddForm from "../components/employees-add-form/employees-add-form"
 
-const App = () => {
-  const data = [
-    { id: 1, name: "Arnold P.", salary: 250, increase: true },
-    { id: 2, name: "Eva A.", salary: 450, increase: false },
-    { id: 3, name: "John N.", salary: 350, increase: false },
-    { id: 4, name: "Kolin F.", salary: 380, increase: true },
-  ]
+class App extends Component {
+  constructor(props) {
+    super(props)
 
+    this.state = {
+      data: [
+        { id: 1, name: "Arnold P.", salary: 250, increase: true },
+        { id: 2, name: "Eva A.", salary: 450, increase: false },
+        { id: 3, name: "John N.", salary: 350, increase: false },
+        { id: 4, name: "Kolin F.", salary: 380, increase: true },
+      ], 
+    }
+  }
 
-  return (
-    <div className="app">
-      <AppInfo />
+		onDeleteItem = (id) => {
+			this.setState(({data}) => {
+				return {data: data.filter(el => el.id !== id)}
+			})
+		}
 
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
+  render() {
+		const {data} = this.state
+    return (
+      <div className="app">
+        <AppInfo />
+
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+        </div>
+
+        <EmployeesList data={data} onDelete={this.onDeleteItem}/>
+        <EmployeesAddForm />
       </div>
-
-      <EmployeesList data={data} />
-      <EmployeesAddForm />
-    </div>
-  )
+    )
+  }
 }
 
 export default App
--- src/components/employees-list/employees-list.jsx
+++ src/components/employees-list/employees-list.jsx
@@ -2,10 +2,10 @@ import React from "react"
 import EmployeesListItem from "../employees-list-item/employees-list-item"
 import "./employees-list.css"
 
-const EmployeesList = ({ data }) => {
+const EmployeesList = ({ data, onDelete }) => {
   const elements = data.map((el) => {
     const { id, ...remainingElements } = el
-    return <EmployeesListItem key={id} {...remainingElements} />
+    return <EmployeesListItem key={id} {...remainingElements} onDelete={() => onDelete(id)} />
   })
 
   return <ul className="app-list list-group">{elements}</ul>
